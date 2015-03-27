fcaccaf559cb397ff4fdc013ec3f8f1f047b8bbb
Eric Newport <kethinov@gmail.com>
2015-03-27 10:58:52 -0400

    suppress minification if noMinify param is present

--- roosevelt-closure.js
+++ roosevelt-closure.js
@@ -1,16 +1,28 @@
 'use strict';
 
-var closureCompiler = require('closurecompiler');
+var closureCompiler = require('closurecompiler'),
+    fs = require('fs');
 
 module.exports = {
   parse: function(app, fileName, callback) {
-    closureCompiler.compile([app.get('jsPath') + fileName], app.get('params').jsCompiler.params || {
-        compilation_level: 'ADVANCED_OPTIMIZATIONS'
-      },
-      function(err, newJs) {
-        var newFile = app.get('jsCompiledOutput') + fileName;
-        callback(err, newFile, newJs);
-      }
-    );
+    var newFile = app.get('jsCompiledOutput') + fileName, newJs;
+
+    // disable minify if noMinify param is present in roosevelt
+    if (!app.get('params').noMinify) {
+      fs.createReadStream(app.get('jsPath') + fileName).pipe(fs.createWriteStream(newFile));
+      newJs = fs.readFileSync(app.get('jsPath') + fileName, 'utf-8');
+      callback(null, newFile, newJs);
+    }
+    
+    // do the compression
+    else {
+      closureCompiler.compile([app.get('jsPath') + fileName], app.get('params').jsCompiler.params || {
+          compilation_level: 'ADVANCED_OPTIMIZATIONS'
+        },
+        function(err, newJs) {
+          callback(err, newFile, newJs);
+        }
+      );
+    }
   }
 };
