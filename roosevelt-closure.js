3f805cf357046c9665dc4be1802b108de5b10c83
Eric Newport <kethinov@gmail.com>
2015-07-30 23:31:25 -0400

    simplify api

--- roosevelt-closure.js
+++ roosevelt-closure.js
@@ -4,23 +4,23 @@ var closureCompiler = require('closurecompiler'),
     fs = require('fs');
 
 module.exports = {
-  parse: function(app, fileName, newFile, callback) {
+  parse: function(app, fileName, filePath, callback) {
     var newJs;
 
     // disable minify if noMinify param is present in roosevelt
     if (app.get('params').noMinify) {
-      fs.createReadStream(app.get('jsPath') + fileName).pipe(fs.createWriteStream(newFile));
+      fs.createReadStream(app.get('jsPath') + fileName).pipe(fs.createWriteStream(filePath));
       newJs = fs.readFileSync(app.get('jsPath') + fileName, 'utf-8');
-      callback(null, newFile, newJs);
+      callback(null, newJs);
     }
-    
+
     // do the compression
     else {
       closureCompiler.compile([app.get('jsPath') + fileName], app.get('params').jsCompiler.params || {
           compilation_level: 'ADVANCED_OPTIMIZATIONS'
         },
         function(err, newJs) {
-          callback(err, newFile, newJs);
+          callback(err, newJs);
         }
       );
     }
