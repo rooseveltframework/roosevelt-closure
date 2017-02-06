7a3cfd68d9b9d597a8858de1d8574a7fcc111068
Eric Newport <kethinov@gmail.com>
2017-02-06 01:08:46 -0500

    half finished rewrite

--- roosevelt-closure.js
+++ roosevelt-closure.js
@@ -1,11 +1,10 @@
-'use strict';
-
-var closureCompiler = require('closurecompiler'),
+var closureCompiler = require('google-closure-compiler-js').compile,
     fs = require('fs');
 
 module.exports = {
   parse: function(app, fileName, filePath, callback) {
-    var newJs;
+    var newJs,
+        params;
 
     // disable minify if noMinify param is present in roosevelt
     if (app.get('params').noMinify) {
@@ -16,13 +15,45 @@ module.exports = {
 
     // do the compression
     else {
-      closureCompiler.compile([app.get('jsPath') + fileName], app.get('params').jsCompiler.params || {
-          compilation_level: 'ADVANCED_OPTIMIZATIONS'
+      console.log('params...');
+      console.log(app.get('params').jsCompiler.params);
+
+      app.get('params').jsCompiler.params = {
+        compilationLevel: 'ADVANCED',
+        warningLevel: 'VERBOSE'
+      };
+      console.log(app.get('params').jsCompiler.params);
+
+      const flags = {
+        languageIn: 'ECMASCRIPT6',
+        languageOut: 'ECMASCRIPT5',
+        compilationLevel: 'ADVANCED',
+        warningLevel: 'VERBOSE',
+        jsCode: [{src: 'var color = \'#eee\'; document.body.style.backgroundColor = color;'}],
+      };
+      const out = closureCompiler(flags);
+
+      console.info(out.compiledCode);
+
+      params = app.get('params').jsCompiler.params || {
+        compilationLevel: 'ADVANCED'
+      };
+
+      console.log('src: ', fs.readFileSync(app.get('jsPath') + fileName, 'utf8'));
+
+      params.jsCode = [{src: 'const x = 1 + 2;'}];
+
+      console.log(closureCompiler);
+      console.log(params);
+      console.log(closureCompiler(params));
+
+      /*closureCompiler.compile([app.get('jsPath') + fileName], app.get('params').jsCompiler.params || {
+          compilation_level: 'ADVANCED'
         },
         function(err, newJs) {
           callback(err, newJs);
         }
-      );
+      );*/
     }
   }
 };
